409d02992d9e1c48558a342cba48966ba82260e6
Dulitha Ranatunga <dulitharanatunga@gmail.com>
2020-12-29 13:06:39 +1100

    Remove storage and most of the test app, add more complicated data model (unused)

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,46 +1,862 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createTodo = /* GraphQL */ `
-  mutation CreateTodo(
-    $input: CreateTodoInput!
-    $condition: ModelTodoConditionInput
+export const createComment = /* GraphQL */ `
+  mutation CreateComment(
+    $input: CreateCommentInput!
+    $condition: ModelCommentConditionInput
   ) {
-    createTodo(input: $input, condition: $condition) {
+    createComment(input: $input, condition: $condition) {
       id
-      name
-      description
-      image
+      content
       createdAt
       updatedAt
     }
   }
 `;
-export const updateTodo = /* GraphQL */ `
-  mutation UpdateTodo(
-    $input: UpdateTodoInput!
-    $condition: ModelTodoConditionInput
+export const updateComment = /* GraphQL */ `
+  mutation UpdateComment(
+    $input: UpdateCommentInput!
+    $condition: ModelCommentConditionInput
   ) {
-    updateTodo(input: $input, condition: $condition) {
+    updateComment(input: $input, condition: $condition) {
       id
-      name
-      description
-      image
+      content
       createdAt
       updatedAt
     }
   }
 `;
-export const deleteTodo = /* GraphQL */ `
-  mutation DeleteTodo(
-    $input: DeleteTodoInput!
-    $condition: ModelTodoConditionInput
+export const deleteComment = /* GraphQL */ `
+  mutation DeleteComment(
+    $input: DeleteCommentInput!
+    $condition: ModelCommentConditionInput
   ) {
-    deleteTodo(input: $input, condition: $condition) {
+    deleteComment(input: $input, condition: $condition) {
+      id
+      content
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createEvent = /* GraphQL */ `
+  mutation CreateEvent(
+    $input: CreateEventInput!
+    $condition: ModelEventConditionInput
+  ) {
+    createEvent(input: $input, condition: $condition) {
       id
       name
-      description
-      image
+      trigger
+      done
+      comments {
+        id
+        content
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateEvent = /* GraphQL */ `
+  mutation UpdateEvent(
+    $input: UpdateEventInput!
+    $condition: ModelEventConditionInput
+  ) {
+    updateEvent(input: $input, condition: $condition) {
+      id
+      name
+      trigger
+      done
+      comments {
+        id
+        content
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteEvent = /* GraphQL */ `
+  mutation DeleteEvent(
+    $input: DeleteEventInput!
+    $condition: ModelEventConditionInput
+  ) {
+    deleteEvent(input: $input, condition: $condition) {
+      id
+      name
+      trigger
+      done
+      comments {
+        id
+        content
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createTask = /* GraphQL */ `
+  mutation CreateTask(
+    $input: CreateTaskInput!
+    $condition: ModelTaskConditionInput
+  ) {
+    createTask(input: $input, condition: $condition) {
+      id
+      name
+      next {
+        id
+        name
+        next {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        comments {
+          id
+          content
+          createdAt
+          updatedAt
+        }
+        blocker {
+          ... on Task {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          ... on Event {
+            id
+            name
+            trigger
+            done
+            createdAt
+            updatedAt
+          }
+        }
+        parents {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        status
+        createdAt
+        updatedAt
+      }
+      comments {
+        id
+        content
+        createdAt
+        updatedAt
+      }
+      blocker {
+        ... on Task {
+          id
+          name
+          next {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          comments {
+            id
+            content
+            createdAt
+            updatedAt
+          }
+          blocker {
+            ... on Task {
+              id
+              name
+              status
+              createdAt
+              updatedAt
+            }
+            ... on Event {
+              id
+              name
+              trigger
+              done
+              createdAt
+              updatedAt
+            }
+          }
+          parents {
+            id
+            name
+            createdAt
+            updatedAt
+          }
+          status
+          createdAt
+          updatedAt
+        }
+        ... on Event {
+          id
+          name
+          trigger
+          done
+          comments {
+            id
+            content
+            createdAt
+            updatedAt
+          }
+          createdAt
+          updatedAt
+        }
+      }
+      parents {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateTask = /* GraphQL */ `
+  mutation UpdateTask(
+    $input: UpdateTaskInput!
+    $condition: ModelTaskConditionInput
+  ) {
+    updateTask(input: $input, condition: $condition) {
+      id
+      name
+      next {
+        id
+        name
+        next {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        comments {
+          id
+          content
+          createdAt
+          updatedAt
+        }
+        blocker {
+          ... on Task {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          ... on Event {
+            id
+            name
+            trigger
+            done
+            createdAt
+            updatedAt
+          }
+        }
+        parents {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        status
+        createdAt
+        updatedAt
+      }
+      comments {
+        id
+        content
+        createdAt
+        updatedAt
+      }
+      blocker {
+        ... on Task {
+          id
+          name
+          next {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          comments {
+            id
+            content
+            createdAt
+            updatedAt
+          }
+          blocker {
+            ... on Task {
+              id
+              name
+              status
+              createdAt
+              updatedAt
+            }
+            ... on Event {
+              id
+              name
+              trigger
+              done
+              createdAt
+              updatedAt
+            }
+          }
+          parents {
+            id
+            name
+            createdAt
+            updatedAt
+          }
+          status
+          createdAt
+          updatedAt
+        }
+        ... on Event {
+          id
+          name
+          trigger
+          done
+          comments {
+            id
+            content
+            createdAt
+            updatedAt
+          }
+          createdAt
+          updatedAt
+        }
+      }
+      parents {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteTask = /* GraphQL */ `
+  mutation DeleteTask(
+    $input: DeleteTaskInput!
+    $condition: ModelTaskConditionInput
+  ) {
+    deleteTask(input: $input, condition: $condition) {
+      id
+      name
+      next {
+        id
+        name
+        next {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        comments {
+          id
+          content
+          createdAt
+          updatedAt
+        }
+        blocker {
+          ... on Task {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          ... on Event {
+            id
+            name
+            trigger
+            done
+            createdAt
+            updatedAt
+          }
+        }
+        parents {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        status
+        createdAt
+        updatedAt
+      }
+      comments {
+        id
+        content
+        createdAt
+        updatedAt
+      }
+      blocker {
+        ... on Task {
+          id
+          name
+          next {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          comments {
+            id
+            content
+            createdAt
+            updatedAt
+          }
+          blocker {
+            ... on Task {
+              id
+              name
+              status
+              createdAt
+              updatedAt
+            }
+            ... on Event {
+              id
+              name
+              trigger
+              done
+              createdAt
+              updatedAt
+            }
+          }
+          parents {
+            id
+            name
+            createdAt
+            updatedAt
+          }
+          status
+          createdAt
+          updatedAt
+        }
+        ... on Event {
+          id
+          name
+          trigger
+          done
+          comments {
+            id
+            content
+            createdAt
+            updatedAt
+          }
+          createdAt
+          updatedAt
+        }
+      }
+      parents {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createGrouping = /* GraphQL */ `
+  mutation CreateGrouping(
+    $input: CreateGroupingInput!
+    $condition: ModelGroupingConditionInput
+  ) {
+    createGrouping(input: $input, condition: $condition) {
+      id
+      name
+      todos {
+        id
+        name
+        next {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        comments {
+          id
+          content
+          createdAt
+          updatedAt
+        }
+        blocker {
+          ... on Task {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          ... on Event {
+            id
+            name
+            trigger
+            done
+            createdAt
+            updatedAt
+          }
+        }
+        parents {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        status
+        createdAt
+        updatedAt
+      }
+      children {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      parent {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateGrouping = /* GraphQL */ `
+  mutation UpdateGrouping(
+    $input: UpdateGroupingInput!
+    $condition: ModelGroupingConditionInput
+  ) {
+    updateGrouping(input: $input, condition: $condition) {
+      id
+      name
+      todos {
+        id
+        name
+        next {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        comments {
+          id
+          content
+          createdAt
+          updatedAt
+        }
+        blocker {
+          ... on Task {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          ... on Event {
+            id
+            name
+            trigger
+            done
+            createdAt
+            updatedAt
+          }
+        }
+        parents {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        status
+        createdAt
+        updatedAt
+      }
+      children {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      parent {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteGrouping = /* GraphQL */ `
+  mutation DeleteGrouping(
+    $input: DeleteGroupingInput!
+    $condition: ModelGroupingConditionInput
+  ) {
+    deleteGrouping(input: $input, condition: $condition) {
+      id
+      name
+      todos {
+        id
+        name
+        next {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        comments {
+          id
+          content
+          createdAt
+          updatedAt
+        }
+        blocker {
+          ... on Task {
+            id
+            name
+            status
+            createdAt
+            updatedAt
+          }
+          ... on Event {
+            id
+            name
+            trigger
+            done
+            createdAt
+            updatedAt
+          }
+        }
+        parents {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        status
+        createdAt
+        updatedAt
+      }
+      children {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      parent {
+        id
+        name
+        todos {
+          id
+          name
+          status
+          createdAt
+          updatedAt
+        }
+        children {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        parent {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
       createdAt
       updatedAt
     }
